1d836b9b6a9e5238f1a03f3c940adcabdfb36885
Rahul Mahato <mahatorahul025@gmail.com>
2024-07-25 12:36:13 +0530

    added landing animation

---
+++ script.js
@@ -0,0 +1,47 @@
+const circle = document.querySelector(".cirlce");
+
+const loaderText = document.querySelector(".loader-container h1");
+
+const loaderContainerClass = ".loader-container";
+
+function loaderAnimation() {
+  const tl = gsap.timeline();
+  tl.from(".loader-container .circle", {
+    scale: 0,
+    duration: 5,
+    delay: 0.8,
+  });
+
+  tl.from(
+    ".loader-container h1",
+    {
+      opacity: 0,
+      fontSize: "0vw",
+      duration: 2,
+      delay: 0.4,
+      onStart: () => {
+        let progress = 0;
+        const interval = setInterval(() => {
+          if (progress === 100) clearInterval(interval);
+          loaderText.textContent = `${progress++}%`;
+        }, 40);
+      },
+    },
+    "<"
+  );
+
+  tl.to(".loader-container", {
+    opacity: 0,
+  });
+
+  tl.to(".home .circle", {
+    scale: 0,
+    duration: 1.6,
+  });
+
+  tl.to(".loader-container, .home .circle", {
+    display: "none",
+  });
+}
+
+loaderAnimation();
