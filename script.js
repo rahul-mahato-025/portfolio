c0a8cfc75cc6d6cc972879d24b0afc9980067567
Rahul Mahato <mahatorahul025@gmail.com>
2024-07-28 23:21:58 +0530

    completed skills section

--- script.js
+++ script.js
@@ -1,65 +1,18 @@
 const circle = document.querySelector(".cirlce");
 
 const aboutTextParagraphs = document.querySelectorAll(".about-text p");
-
 const aboutHeadings = document.querySelectorAll(".about-heading h1");
-
-const aboutHeadingDiv = document.querySelector(".about-heading div");
+const aboutHeadingDiv = document.querySelector(".about-heading .heading-text");
+const projectsHeadingDiv = document.querySelector(".projects-intro");
+const projectsHeading = document.querySelector(".projects-intro h1");
+const skillsHeadingDiv = document.querySelector(
+  ".skills-heading .heading-text"
+);
+const skillsHeadings = document.querySelectorAll(".skills-heading h1");
 
 const loaderText = document.querySelector(".loader-container h1");
 
-function loaderAnimation() {
-  const tl = gsap.timeline();
-  tl.from(".loader-container .circle", {
-    scale: 0,
-    duration: 5,
-    delay: 0.8,
-  });
-
-  tl.from(
-    ".loader-container h1",
-    {
-      opacity: 0,
-      fontSize: "0vw",
-      duration: 2,
-      delay: 0.4,
-      onStart: () => {
-        let progress = 0;
-        const interval = setInterval(() => {
-          if (progress === 100) clearInterval(interval);
-          loaderText.textContent = `${progress++}%`;
-        }, 40);
-      },
-    },
-    "<"
-  );
-
-  tl.to(".loader-container", {
-    opacity: 0,
-  });
-
-  tl.to(".home .circle", {
-    scale: 0,
-    duration: 1,
-  });
-
-  tl.to(".loader-container, .home .circle", {
-    display: "none",
-  });
-
-  tl.from(".socials .icon", {
-    opacity: 0,
-    duration: 0.2,
-    stagger: 0.2,
-  });
-
-  tl.from(".text h1", {
-    y: 150,
-    opacity: 0,
-    duration: 0.2,
-    stagger: 0.3,
-  });
-}
+gsap.registerPlugin(ScrollTrigger);
 
 function addSpanToLetters(text) {
   let spanText = "";
@@ -95,6 +48,103 @@ function triggerTransformTextAnimation(container, event, text1, text2) {
   });
 }
 
+function enableLocomotiveScroll() {
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("main"),
+    smooth: true,
+  });
+
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  ScrollTrigger.scrollerProxy("main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    },
+
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+
+    pinType: document.querySelector("main").style.transform
+      ? "transform"
+      : "fixed",
+  });
+
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  ScrollTrigger.refresh();
+}
+
+function loaderAnimation() {
+  const tl = gsap.timeline();
+
+  tl.from(".loader-container .circle", {
+    scale: 0,
+    duration: 5,
+    delay: 0.8,
+  });
+
+  tl.from(
+    ".loader-container h1",
+    {
+      opacity: 0,
+      fontSize: "0vw",
+      duration: 2,
+      delay: 0.4,
+      onStart: () => {
+        let progress = 0;
+
+        const interval = setInterval(() => {
+          if (progress === 100) {
+            clearInterval(interval);
+          }
+
+          loaderText.textContent = `${progress++}%`;
+        }, 40);
+      },
+    },
+    "<"
+  );
+
+  tl.to(".loader-container", {
+    opacity: 0,
+  });
+
+  tl.to(".home .circle", {
+    scale: 0,
+    duration: 1,
+  });
+
+  tl.to(".loader-container, .home .circle", {
+    display: "none",
+  });
+
+  tl.from(".socials .icon", {
+    opacity: 0,
+    duration: 0.2,
+    stagger: 0.2,
+  });
+
+  tl.from(".home-text h1", {
+    y: 150,
+    opacity: 0,
+    duration: 0.2,
+    stagger: 0.3,
+  });
+
+  tl.from(".about", {
+    opacity: 0,
+    duration: 1,
+  });
+}
+
 function aboutPageAnimation() {
   aboutTextParagraphs.forEach((paragraph) => {
     paragraph.innerHTML = addSpanToLetters(paragraph.textContent);
@@ -105,9 +155,10 @@ function aboutPageAnimation() {
     stagger: 0.5,
     scrollTrigger: {
       trigger: ".about-text span",
+      scroller: "main",
       start: "top 100%",
       end: "top 20%",
-      scrub: 3,
+      scrub: 2,
     },
   });
 
@@ -130,5 +181,87 @@ function aboutPageAnimation() {
   );
 }
 
-// loaderAnimation();
+function projectsSectionAnimation() {
+  projectsHeading.innerHTML = addSpanToLetters(projectsHeading.textContent);
+
+  projectsHeading.addEventListener("mouseenter", function () {
+    gsap.to(".projects-intro h1 span", {
+      fontSize: "7vw",
+      duration: 0.2,
+      stagger: 0.03,
+    });
+  });
+
+  projectsHeading.addEventListener("mouseleave", function () {
+    gsap.to(".projects-intro h1 span", {
+      fontSize: "5vw",
+      duration: 0.2,
+      stagger: 0.03,
+    });
+  });
+
+  gsap.to(".scroll-item", {
+    transform: "translateX(-100%)",
+    scrollTrigger: {
+      trigger: ".projects",
+      scroller: "main",
+      pin: true,
+      scrub: 3,
+      start: "top 0%",
+      end: "top -100%",
+    },
+  });
+}
+
+function skillsAnimation() {
+  const tl = gsap.timeline();
+  tl.from(".skill-item", {
+    opacity: 0,
+    duration: 2,
+    stagger: 0.5,
+    scrollTrigger: {
+      trigger: ".skills-text",
+      scroller: "main",
+      start: "top: 50%",
+      end: "top 20%",
+      scrub: 2,
+    },
+  });
+
+  tl.from(".skill-item .underline", {
+    width: 0,
+    duration: 1,
+    stagger: 0.5,
+    scrollTrigger: {
+      trigger: ".skill-item .underline",
+      scroller: "main",
+      start: "top: 100%",
+      end: "top 20%",
+      scrub: 2,
+    },
+  });
+
+  skillsHeadings.forEach((skillsHeading) => {
+    skillsHeading.innerHTML = addSpanToLetters(skillsHeading.textContent);
+  });
+
+  triggerTransformTextAnimation(
+    skillsHeadingDiv,
+    "mouseenter",
+    ".skills-heading-1 span",
+    ".skills-heading-2 span"
+  );
+
+  triggerTransformTextAnimation(
+    skillsHeadingDiv,
+    "mouseleave",
+    ".skills-heading-2 span",
+    ".skills-heading-1 span"
+  );
+}
+
+enableLocomotiveScroll();
+loaderAnimation();
 aboutPageAnimation();
+projectsSectionAnimation();
+skillsAnimation();
