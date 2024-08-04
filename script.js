8cbab845707fee86dec1e7700e4deea0acde28bb
Rahul Mahato <mahatorahul025@gmail.com>
2024-08-04 11:08:34 +0530

    added media queries for mobile and laptop

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const circle = document.querySelector(".cirlce");
-
+const homeTextDiv = document.querySelectorAll(".home-text");
+const portfolioText = document.querySelector(".portfolio");
 const aboutTextParagraphs = document.querySelectorAll(".about-text p");
 const aboutHeadings = document.querySelectorAll(".about-heading h1");
 const aboutHeadingDiv = document.querySelector(".about-heading .heading-text");
@@ -13,6 +14,7 @@ const skillsHeadings = document.querySelectorAll(".skills-heading h1");
 const loaderText = document.querySelector(".loader-container h1");
 
 gsap.registerPlugin(ScrollTrigger);
+ScrollTrigger.normalizeScroll(true);
 
 function addSpanToLetters(text) {
   let spanText = "";
@@ -23,28 +25,13 @@ function addSpanToLetters(text) {
   return spanText;
 }
 
-function triggerTransformTextAnimation(container, event, text1, text2) {
-  const tl = gsap.timeline();
-
+function textColorChangeAnimation(container, event, element, color) {
   container.addEventListener(event, function () {
-    if (tl.isActive()) return;
-
-    tl.to(text1, {
-      opacity: 0,
-      duration: 0.02,
-      stagger: 0.02,
+    gsap.to(element, {
+      color: color,
+      duration: 0.1,
+      stagger: 0.03,
     });
-
-    tl.to(
-      text2,
-      {
-        opacity: 1,
-        duration: 0.02,
-        // delay: 0.022,
-        stagger: 0.02,
-      },
-      "<"
-    );
   });
 }
 
@@ -83,6 +70,7 @@ function enableLocomotiveScroll() {
 }
 
 function loaderAnimation() {
+  portfolioText.innerHTML = addSpanToLetters(portfolioText.textContent);
   const tl = gsap.timeline();
 
   tl.from(".loader-container .circle", {
@@ -143,6 +131,15 @@ function loaderAnimation() {
     opacity: 0,
     duration: 1,
   });
+
+  tl.to(".portfolio span", {
+    color: "#ff7d7b",
+    duration: 0.5,
+    repeat: -1,
+    stagger: 0.02,
+    ease: Power2.easeInOut,
+    yoyo: true,
+  });
 }
 
 function aboutPageAnimation() {
@@ -166,18 +163,18 @@ function aboutPageAnimation() {
     heading.innerHTML = addSpanToLetters(heading.textContent);
   });
 
-  triggerTransformTextAnimation(
+  textColorChangeAnimation(
     aboutHeadingDiv,
     "mouseenter",
     ".about-heading-1 span",
-    ".about-heading-2 span"
+    "#ff7d7b"
   );
 
-  triggerTransformTextAnimation(
+  textColorChangeAnimation(
     aboutHeadingDiv,
     "mouseleave",
-    ".about-heading-2 span",
-    ".about-heading-1 span"
+    ".about-heading-1 span",
+    "#fefefe"
   );
 }
 
@@ -186,7 +183,7 @@ function projectsSectionAnimation() {
 
   projectsHeading.addEventListener("mouseenter", function () {
     gsap.to(".projects-intro h1 span", {
-      fontSize: "7vw",
+      fontSize: "6vw",
       duration: 0.2,
       stagger: 0.03,
     });
@@ -206,23 +203,25 @@ function projectsSectionAnimation() {
       trigger: ".projects",
       scroller: "main",
       pin: true,
-      scrub: 3,
+      scrub: 2,
       start: "top 0%",
       end: "top -100%",
+      // pinSpacing: false,
     },
   });
 }
 
 function skillsAnimation() {
   const tl = gsap.timeline();
+
   tl.from(".skill-item", {
     opacity: 0,
     duration: 2,
     stagger: 0.5,
     scrollTrigger: {
-      trigger: ".skills-text",
+      trigger: ".skills-item",
       scroller: "main",
-      start: "top: 50%",
+      start: "top 50%",
       end: "top 20%",
       scrub: 2,
     },
@@ -233,10 +232,11 @@ function skillsAnimation() {
     duration: 1,
     stagger: 0.5,
     scrollTrigger: {
-      trigger: ".skill-item .underline",
+      trigger: ".skills",
       scroller: "main",
-      start: "top: 100%",
+      start: "top 100%",
       end: "top 20%",
+      // markers: true,
       scrub: 2,
     },
   });
@@ -245,18 +245,18 @@ function skillsAnimation() {
     skillsHeading.innerHTML = addSpanToLetters(skillsHeading.textContent);
   });
 
-  triggerTransformTextAnimation(
+  textColorChangeAnimation(
     skillsHeadingDiv,
     "mouseenter",
     ".skills-heading-1 span",
-    ".skills-heading-2 span"
+    "#ff7d7b"
   );
 
-  triggerTransformTextAnimation(
+  textColorChangeAnimation(
     skillsHeadingDiv,
     "mouseleave",
-    ".skills-heading-2 span",
-    ".skills-heading-1 span"
+    ".skills-heading-1 span",
+    "#fefefe"
   );
 }
 
